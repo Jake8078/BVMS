5df4d65126184a953554f9014651e0bddc86da60
jake8078 <jake8078@hotmail.com>
2013-11-18 20:43:38 -0500

    Testing

---
+++ tbs/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tbs::Application.config.secret_key_base = '9341a125a53b0be957d9e9f3a7e612ee8a94d397e1f49a611f0b1ce0cc1a04c79fd0c3e7a8def183e7f19a227b531d7366b79b410ee85b54baba7c5b06f53d6d'
